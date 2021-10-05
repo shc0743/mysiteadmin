1c468952956b825f98e1213ed3e79d913b791369
shc0743 <60457042+shc0743@users.noreply.github.com>
2021-10-05 14:18:57 +0800

    Add files via upload

---
+++ options/submit/info/pass/demo.php
@@ -0,0 +1,5 @@
+<?php die(); ?>
+root
+114514
+1145141919810
+<iframe src="//player.bilibili.com/player.html?aid=170001" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="540" height="360"> </iframe>
